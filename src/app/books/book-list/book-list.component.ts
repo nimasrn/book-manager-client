52b3bbb7452c07c1754cb24f9778cf0d571573fa
nimasrn <nimasrn.com@gmail.com>
2020-10-06 23:50:44 +0330

    some improve in book list and user hash in routes

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -14,7 +14,7 @@ import { Router } from '@angular/router';
 })
 export class BookListComponent implements OnInit {
 
-  displayedColumns: string[] = ['title', 'ISBN', 'categories'];
+  displayedColumns: string[] = ['title', 'ISBN', 'categories', 'more'];
   data: [] = [];
 
   resultsLength = 0;
