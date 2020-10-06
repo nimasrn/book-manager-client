0f681e2e154e92c338b235d7fb84c0ed19907a88
nimasrn <nimasrn.com@gmail.com>
2020-10-06 23:57:21 +0330

    fix prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://book-management-server.herokuapp.com/',
+  api_url: 'https://book-management-server.herokuapp.com',
   version: '0.1.1'
 };
